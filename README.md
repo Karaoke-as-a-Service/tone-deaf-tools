ef45caea9ada15a37eccb0868f9be535ffbe493b
luto <m@luto.at>
2019-09-20 20:41:26 +0200

    add character_analysis.py

--- README.md
+++ README.md
@@ -85,6 +85,7 @@ Scenario: your collection has mixed `#LANGUAGE` attributes like  "English",
 * [check_health.py](#check_healthpy)
 * [fix_file_links.py](#fix_file_linkspy)
 * [recode_language.py](#recode_languagepy)
+* [character_analysis.py](#character_analysispy)
 * [get_lyrics.py](#get_lyricspy)
 * [debug_encoding.py](#debug_encodingpy)
 * [normalize_line_endings.py](#normalize_line_endingspy)
@@ -301,6 +302,23 @@ optional arguments:
   -h, --help  show this help message and exit
   --dry-run   just find the encoding, do not change the file.
   --verbose
+
+```
+
+### character_analysis.py
+
+```console
+$ ./character_analysis.py --help
+usage: character_analysis.py [-h] files [files ...]
+
+For a list of files, collect all characters in artist, title and lyrics. Print
+the count of each character, the filename as well as the artist/title.
+
+positional arguments:
+  files
+
+optional arguments:
+  -h, --help  show this help message and exit
 
 ```
 
---
+++ character_analysis.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+from collections import Counter
+
+from _utils import get_artisttitle, get_lyrics
+
+
+HELP='''
+For a list of files, collect all characters in artist, title and lyrics. Print
+the count of each character, the filename as well as the artist/title.
+'''
+
+
+def count_characters(text):
+    artisttitle = get_artisttitle(text)
+    lyrics = get_lyrics(text)
+    return Counter(f'{artisttitle}{lyrics}')
+
+
+def print_character_frequencies(path):
+    with open(path) as f:
+        text = f.read()
+
+    artisttitle = get_artisttitle(text)
+    frequencies = count_characters(text)
+    del frequencies[' ']
+
+    print('\n'.join(f'{char} {count}' for char, count in sorted(frequencies.items(), key=lambda x: -x[1])))
+    print(path)
+    print(artisttitle)
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description=HELP)
+    parser.add_argument('files', nargs='+')
+    args = parser.parse_args(argv)
+
+    for path in args.files:
+        print_character_frequencies(path)
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
